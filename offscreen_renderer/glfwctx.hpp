529f04b50cfb470f5084666dc97a65caa4a0b9d6
0xAA55 <weixiaobao213@qq.com>
2023-06-25 09:17:53 +0800

    Simply wrap GLFW library for creating an offscreen context.

---
+++ offscreen_renderer/glfwctx.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <stdexcept>
+#include <memory>
+#include "glcontext.hpp"
+
+struct GLFWwindow;
+
+namespace RenderTaskSolver
+{
+    using namespace GLObjects;
+
+    class glfwCreateWindowFailure : public std::runtime_error
+    {
+    public:
+        glfwCreateWindowFailure(std::string what) noexcept;
+    };
+
+    class HiddenGLFWWindow
+    {
+    protected:
+        GLFWwindow* window = nullptr;
+        std::unique_ptr<Context> glCtx;
+
+    public:
+        HiddenGLFWWindow();
+        ~HiddenGLFWWindow();
+
+        Context& GetOpenGLContext();
+    };
+}
