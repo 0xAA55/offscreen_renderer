141e976f6a0ccdef4deff25380aec4462ee43c0b
0xAA55 <weixiaobao213@qq.com>
2023-07-02 19:05:58 +0800

    Parse commandline options for task solver

--- offscreen_renderer/main.cpp
+++ offscreen_renderer/main.cpp
@@ -9,14 +9,17 @@ int main(int argc, char **argv)
 {
     if (argc <= 1)
     {
-        std::cout << "Usage: " << argv[0] << "<config file>" << std::endl;
+        std::cout << "Usage: " << argv[0] << "<config file> [options]" << std::endl;
         return 1;
     }
+
+    std::set<std::string> Options;
+    for (int i = 2; i < argc; i++) Options.insert(argv[i]);
 
     HiddenGLFWWindow w;
     try
     {
-        TaskSolver s(w.GetOpenGLContext(), argv[1]);
+        TaskSolver s(w.GetOpenGLContext(), argv[1], Options);
         s.SolveTasks();
     }
     catch (const std::runtime_error& e)
