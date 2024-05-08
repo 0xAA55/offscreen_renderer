8401793cacb55cbee054a6e5f070d23cfcb0b19a
0xAA55 <weixiaobao213@qq.com>
2024-05-08 17:06:43 +0800

    使上下文的使用可以通过命令行参数 `-glfw` 或 `-egl` 来挑选，前者在 Windows 正常，在 Linux 依赖一个 X Server；后者使用无头 OpenGL ES 3.0。

--- offscr/main.cpp
+++ offscr/main.cpp
@@ -1,9 +1,38 @@
 ﻿#include <iostream>
 
 #include <liboffscr/eglctx.hpp>
+#include <liboffscr/glfwctx.hpp>
 #include <liboffscr/tasksolver.hpp>
 
 using namespace RenderTaskSolver;
+
+class GLCtxBase
+{
+public:
+    virtual const Context& GetOpenGLContext() const = 0;
+};
+
+class GLCtxEGL : public GLCtxBase
+{
+    EGLCtx c;
+
+public:
+    virtual const Context& GetOpenGLContext() const override
+    {
+        return c.GetOpenGLContext();
+    }
+};
+
+class GLCtxGLFW : public GLCtxBase
+{
+    HiddenGLFWWindow w;
+
+public:
+    virtual const Context& GetOpenGLContext() const override
+    {
+        return w.GetOpenGLContext();
+    }
+};
 
 int main(int argc, char **argv)
 {
@@ -16,14 +45,19 @@ int main(int argc, char **argv)
     std::set<std::string> Options;
     for (int i = 2; i < argc; i++) Options.insert(argv[i]);
 
-    EGLCtx c;
+    std::unique_ptr<GLCtxBase> c = nullptr;
+
+    if (Options.contains("-glfw")) c = std::make_unique<GLCtxGLFW>();
+    else if (Options.contains("-egl")) c = std::make_unique<GLCtxEGL>();
+    if (!c) c = std::make_unique<GLCtxGLFW>();
+
 #if _DEBUG
     if(1)
 #else
     try
 #endif
     {
-        TaskSolver s(c.GetOpenGLContext(), argv[1], Options);
+        TaskSolver s(c->GetOpenGLContext(), argv[1], Options);
         s.SolveTasks();
     }
 #if _DEBUG
