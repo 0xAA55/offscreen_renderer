1c29adf5e7906dd2e3c2edd8ed9e8c3d48841d7d
0xAA55 <weixiaobao213@qq.com>
2024-05-08 17:06:40 +0800

    实现 OpenGL ES 3.0 的无头上下文创建能力。

--- liboffscr/eglctx.cpp
+++ liboffscr/eglctx.cpp
@@ -15,6 +15,7 @@
 #  endif
 #endif
 
+
 namespace RenderTaskSolver
 {
 	EGLCreateContextError::EGLCreateContextError(const std::string& what) noexcept :
@@ -30,47 +31,63 @@ namespace RenderTaskSolver
 	EGLCtx::EGLCtx()
 	{
 		static const EGLint configAttribs[] = {
-			EGL_SURFACE_TYPE, EGL_PBUFFER_BIT,
 			EGL_BLUE_SIZE, 8,
 			EGL_GREEN_SIZE, 8,
 			EGL_RED_SIZE, 8,
-			EGL_DEPTH_SIZE, 8,
-			EGL_RENDERABLE_TYPE, EGL_OPENGL_BIT,
+			EGL_DEPTH_SIZE, 24,
+			EGL_RENDERABLE_TYPE, EGL_OPENGL_ES3_BIT,
+			EGL_SURFACE_TYPE, EGL_PBUFFER_BIT,
 			EGL_NONE
 		};
-		auto eglQueryDevicesEXT = reinterpret_cast<PFNEGLQUERYDEVICESEXTPROC>(glGetProcAddress("eglQueryDevicesEXT"));
-		if (!eglQueryDevicesEXT) throw EGLCreateContextError("ERROR: extension eglQueryDevicesEXT not available");
+		static const EGLint pbufferAttribs[] = {
+			EGL_WIDTH, 9,
+			EGL_HEIGHT, 9,
+			EGL_NONE,
+		};
+		static const EGLint attribList[] = {
+			EGL_CONTEXT_MAJOR_VERSION, 3,
+			EGL_CONTEXT_MINOR_VERSION, 0,
+			EGL_NONE,
+		};
+		EGLBoolean ret = 0;
 
-		auto eglGetPlatformDisplayEXT = reinterpret_cast<PFNEGLGETPLATFORMDISPLAYEXTPROC>(eglGetProcAddress("eglGetPlatformDisplayEXT"));
-		if (!eglGetPlatformDisplayEXT) throw EGLCreateContextError("ERROR: extension eglGetPlatformDisplayEXT not available");
-
-		static const int MAX_DEVICES = 16;
-		EGLDeviceEXT devices[MAX_DEVICES];
-		EGLint numDevices;
-
-		eglQueryDevicesEXT(MAX_DEVICES, devices, &numDevices);
-
-		EGLDisplay eglDpy = eglGetPlatformDisplayEXT(EGL_PLATFORM_DEVICE_EXT, devices[0], 0);
+		// 1. Initialize EGL
+		EGLDisplayPtr = std::make_unique<EGLDisplay>(eglGetDisplay(EGL_DEFAULT_DISPLAY));
+		auto& eglDpy = *EGLDisplayPtr;
+		if (!eglDpy) throw EGLCreateContextError("eglGetDisplay(EGL_DEFAULT_DISPLAY) returns NULL.");
 
 		EGLint major, minor;
-
-		eglInitialize(eglDpy, &major, &minor);
+		ret = eglInitialize(eglDpy, &major, &minor);
+		if (!ret) throw EGLCreateContextError("eglInitialize() returns GL_FALSE.");
 
 		// 2. Select appropriate configuration
-		EGLint numConfigs;
-		EGLConfig eglCfg;
+		EGLint numConfigs = 0;
+		EGLConfig eglCfg = nullptr;
 
-		eglChooseConfig(eglDpy, configAttribs, &eglCfg, 1, &numConfigs);
+		ret = eglChooseConfig(eglDpy, configAttribs, &eglCfg, 1, &numConfigs);
+		if (!ret) throw EGLCreateContextError("eglChooseConfig() returns GL_FALSE.");
 
-		// 3. Bind the API 
-		eglBindAPI(EGL_OPENGL_API);
+		// 3. Create a surface
+		EGLSurfacePtr = std::make_unique<EGLSurface>(eglCreatePbufferSurface(eglDpy, eglCfg, pbufferAttribs));
+		auto& eglSurf = *EGLSurfacePtr;
+		if (!eglSurf) throw EGLCreateContextError("eglCreatePbufferSurface() returns NULL.");
 
-		// 4. create the context
-		EGLContext eglCtx = eglCreateContext(eglDpy, eglCfg, EGL_NO_CONTEXT, NULL);
+		// 4. Bind the API
+		ret = eglBindAPI(EGL_OPENGL_API);
 
-		eglMakeCurrent(eglDpy, EGL_NO_SURFACE, EGL_NO_SURFACE, eglCtx);
-		
+		// 5. Create a context and make it current
+		EGLContextPtr = std::make_unique<EGLSurface>(eglCreateContext(eglDpy, eglCfg, EGL_NO_CONTEXT, attribList));
+		auto& eglCtx = *EGLContextPtr;
+		if (!eglCtx) throw EGLCreateContextError("eglCreateContext() returns NULL.");
+
+		ret = eglMakeCurrent(eglDpy, eglSurf, eglSurf, eglCtx);
+
 		glCtx = std::make_unique<Context>(glGetProcAddress);
+	}
+
+	EGLCtx::~EGLCtx()
+	{
+		eglTerminate(*EGLDisplayPtr);
 	}
 
 	const Context& EGLCtx::GetOpenGLContext() const
--- liboffscr/eglctx.hpp
+++ liboffscr/eglctx.hpp
@@ -14,12 +14,20 @@ namespace RenderTaskSolver
         EGLCreateContextError(const std::string& what) noexcept;
     };
 
+    typedef void* EGLDisplay;
+    typedef void* EGLSurface;
+    typedef void* EGLContext;
+
     class EGLCtx
     {
+        std::unique_ptr<EGLDisplay> EGLDisplayPtr;
+        std::unique_ptr<EGLSurface> EGLSurfacePtr;
+        std::unique_ptr<EGLContext> EGLContextPtr;
         std::unique_ptr<Context> glCtx;
 
     public:
         EGLCtx();
+        ~EGLCtx();
 
         const Context& GetOpenGLContext() const;
     };
