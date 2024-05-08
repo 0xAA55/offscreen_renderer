8b692ef7020dc00f7619d4513354b3729c892f3c
0xAA55 <weixiaobao213@qq.com>
2024-05-08 17:06:41 +0800

    使调试时可以看到具体的异常调用栈。

--- offscr/main.cpp
+++ offscr/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char **argv)
 {
     if (argc <= 1)
     {
-        std::cout << "Usage: " << argv[0] << "<config file> [options]" << std::endl;
+        std::cout << "Usage: " << argv[0] << " <config file> [options]" << std::endl;
         return 1;
     }
 
@@ -17,16 +17,23 @@ int main(int argc, char **argv)
     for (int i = 2; i < argc; i++) Options.insert(argv[i]);
 
     EGLCtx c;
+#if _DEBUG
+    if(1)
+#else
     try
+#endif
     {
         TaskSolver s(c.GetOpenGLContext(), argv[1], Options);
         s.SolveTasks();
     }
+#if _DEBUG
+#else
     catch (const std::exception& e)
     {
         std::cerr << e.what();
         return 2;
     }
+#endif
     
     return 0;
 }
