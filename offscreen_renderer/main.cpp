a56af412eda08cf7d24d389489ce9252ee9570b2
0xAA55 <weixiaobao213@qq.com>
2024-05-08 17:06:27 +0800

    added the `int main()`

---
+++ offscreen_renderer/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+#include "glfwctx.hpp"
+#include "tasksolver.hpp"
+
+using namespace RenderTaskSolver;
+
+int main(int argc, char **argv)
+{
+    if (argc <= 1)
+    {
+        std::cout << "Usage: " << argv[0] << "<config file>" << std::endl;
+        return 1;
+    }
+
+    HiddenGLFWWindow w;
+    try
+    {
+        TaskSolver s(w.GetOpenGLContext(), argv[1]);
+        s.SolveTasks();
+    }
+    catch (const std::runtime_error& e)
+    {
+        std::cout << e.what();
+    }
+    
+    return 0;
+}
