61dbbb89bd71f76096e78d981ba39fa2041e4a6c
0xAA55 <weixiaobao213@qq.com>
2024-05-08 17:06:36 +0800

    添加可执行文件工程生成。

---
+++ offscr/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+#include <liboffscr/eglctx.hpp>
+#include <liboffscr/tasksolver.hpp>
+
+using namespace RenderTaskSolver;
+
+int main(int argc, char **argv)
+{
+    if (argc <= 1)
+    {
+        std::cout << "Usage: " << argv[0] << "<config file> [options]" << std::endl;
+        return 1;
+    }
+
+    std::set<std::string> Options;
+    for (int i = 2; i < argc; i++) Options.insert(argv[i]);
+
+    // Using a hidden GLFW window to achieve offscreen rendering.
+    EGLCtx c;
+    try
+    {
+        TaskSolver s(c.GetOpenGLContext(), argv[1], Options);
+        s.SolveTasks();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what();
+        return 2;
+    }
+    
+    return 0;
+}
